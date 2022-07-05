0503c856485224124d994d924e7fe56c5c7ca71a
Stella Cannefax <stella@mystenlabs.com>
2022-07-05 18:01:35 -0500

    initial version of validator info page

--- explorer/client/src/pages/config/AppRoutes.tsx
+++ explorer/client/src/pages/config/AppRoutes.tsx
@@ -9,6 +9,7 @@ import { ObjectResult } from '../object-result/ObjectResult';
 import SearchResult from '../search-result/SearchResult';
 import SearchError from '../searcherror/SearchError';
 import TransactionResult from '../transaction-result/TransactionResult';
+import { ValidatorResult } from '../validators/Validators';
 
 const AppRoutes = () => {
     return (
@@ -17,6 +18,7 @@ const AppRoutes = () => {
             <Route path="/objects/:id" element={<ObjectResult />} />
             <Route path="/transactions/:id" element={<TransactionResult />} />
             <Route path="/addresses/:id" element={<AddressResult />} />
+            <Route path="/validators" element={<ValidatorResult />} />
             <Route path="/search-result/:id" element={<SearchResult />} />
             <Route path="/error/:category/:id" element={<SearchError />} />
             <Route path="*" element={<Navigate to="/" replace={true} />} />
