311dd0246763b762daf004fddee51b3c7fa0e88d
Stella Cannefax <stella@mystenlabs.com>
2022-07-13 16:48:29 -0500

    lint changes

--- explorer/client/src/pages/home/Home.tsx
+++ explorer/client/src/pages/home/Home.tsx
@@ -85,7 +85,7 @@ function HomeAPI() {
                 <LastestTxCard count={results.count} />
             </section>
             <section className="right-item">
-                <TopValidatorsCardAPI/>
+                <TopValidatorsCardAPI />
             </section>
         </div>
     );
